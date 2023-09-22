6f81eebb84c054ddeb958d808213b42a3749ef38
abhay <abhay@gmail.com>
2023-09-22 15:54:47 +0530

    create instances with key pair

--- main.tf
+++ main.tf
@@ -155,3 +155,14 @@ resource "aws_instance" "private_instance" {
 
 
 
+# Outputs for convenience
+output "vpc_id" {
+  value = aws_vpc.main.id
+}
+output "public_subnet_id" {
+  value = aws_subnet.public.id
+}
+
+output "private_subnet_ids" {
+  value = aws_subnet.private.*.id
+}
