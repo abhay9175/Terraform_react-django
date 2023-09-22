1d124bc07298dd829c9c0834868a649e6ec33074
abhay <abhay@gmail.com>
2023-09-22 11:57:02 +0530

    trial

--- main.tf
+++ main.tf
@@ -52,88 +52,88 @@ resource "aws_internet_gateway" "gw" {
   }
 }
 
-# Create NAT Gateway
-resource "aws_nat_gateway" "nat" {
-  count = length(aws_subnet.private)
+# # Create NAT Gateway
+# resource "aws_nat_gateway" "nat" {
+#   count = length(aws_subnet.private)
 
-  subnet_id = aws_subnet.private[count.index].id
-  allocation_id = aws_eip.nat[count.index].id
-}
-
-
+#   subnet_id = aws_subnet.private[count.index].id
+#   allocation_id = aws_eip.nat[count.index].id
+# }
 
 
-# Create a Route Table for Public Subnet
-resource "aws_route_table" "public" {
-  vpc_id = aws_vpc.main.id
 
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.gw.id
-  }
-tags = {
-    Name = "PublicRouteTable"
-  }
-}
 
-# Associate Public Subnet with Public Route Table
-resource "aws_route_table_association" "public" {
-  subnet_id      = aws_subnet.public.id
-  route_table_id = aws_route_table.public.id
-}
+# # Create a Route Table for Public Subnet
+# resource "aws_route_table" "public" {
+#   vpc_id = aws_vpc.main.id
 
-# Create a Route Table for Private Subnets
-resource "aws_route_table" "private" {
-  count  = length(aws_subnet.private)
-  vpc_id = aws_vpc.main.id
+#   route {
+#     cidr_block = "0.0.0.0/0"
+#     gateway_id = aws_internet_gateway.gw.id
+#   }
+# tags = {
+#     Name = "PublicRouteTable"
+#   }
+# }
 
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_nat_gateway.nat[count.index].id
-  }
+# # Associate Public Subnet with Public Route Table
+# resource "aws_route_table_association" "public" {
+#   subnet_id      = aws_subnet.public.id
+#   route_table_id = aws_route_table.public.id
+# }
 
-  tags = {
-    Name = "PrivateRouteTable-${count.index + 1}"
-  }
-}
+# # Create a Route Table for Private Subnets
+# resource "aws_route_table" "private" {
+#   count  = length(aws_subnet.private)
+#   vpc_id = aws_vpc.main.id
 
-# Associate Private Subnets with Private Route Tables
-resource "aws_route_table_association" "private" {
-  count          = length(aws_subnet.private)
-  subnet_id      = aws_subnet.private[count.index].id
-  route_table_id = aws_route_table.private[count.index].id
-  nat_gateway_id        = aws_nat_gateway.nat.id
-  destination_cidr_block = "0.0.0.0/0" # Route all traffic
-}
+#   route {
+#     cidr_block = "0.0.0.0/0"
+#     gateway_id = aws_nat_gateway.nat[count.index].id
+#   }
 
-# Create a Security Group for React-django app
-resource "aws_security_group" "React-django" {
-  name_prefix = "React-django"
-  vpc_id      = aws_vpc.main.id
+#   tags = {
+#     Name = "PrivateRouteTable-${count.index + 1}"
+#   }
+# }
 
-  # Inbound rule
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["10.0.0.0/24"]
-  }
- # Inbound rule
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-# Inbound rule
-ingress {
-    from_port   = 0
-    to_port     = 65535
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+# # Associate Private Subnets with Private Route Tables
+# resource "aws_route_table_association" "private" {
+#   count          = length(aws_subnet.private)
+#   subnet_id      = aws_subnet.private[count.index].id
+#   route_table_id = aws_route_table.private[count.index].id
+#   nat_gateway_id        = aws_nat_gateway.nat.id
+#   destination_cidr_block = "0.0.0.0/0" # Route all traffic
+# }
 
-}
+# # Create a Security Group for React-django app
+# resource "aws_security_group" "React-django" {
+#   name_prefix = "React-django"
+#   vpc_id      = aws_vpc.main.id
+
+#   # Inbound rule
+#   ingress {
+#     from_port   = 22
+#     to_port     = 22
+#     protocol    = "tcp"
+#     cidr_blocks = ["10.0.0.0/24"]
+#   }
+#  # Inbound rule
+#   ingress {
+#     from_port   = 80
+#     to_port     = 80
+#     protocol    = "tcp"
+#     cidr_blocks = ["0.0.0.0/0"]
+#   }
+# # Inbound rule
+# ingress {
+#     from_port   = 0
+#     to_port     = 65535
+#     protocol    = "tcp"
+#     cidr_blocks = ["0.0.0.0/0"]
+#   }
+
+# }
 
 
 
