b66e854e3c8dd4a858b83fd095c27cb0b3e3b895
abhay <abhay@gmail.com>
2023-09-22 12:37:46 +0530

    create routetable for public subnet and private subnet

--- main.tf
+++ main.tf
@@ -105,7 +105,7 @@ resource "aws_route_table_association" "private" {
   count          = length(aws_subnet.private)
   subnet_id      = aws_subnet.private[count.index].id
   route_table_id = aws_route_table.private[count.index].id
-  nat_gateway_id        = aws_nat_gateway.nat.id
+  nat_gateway_id        = aws_nat_gateway.nat[count.index].id
   destination_cidr_block = "0.0.0.0/0" # Route all traffic
 }
 
