bdcf66de3269ecf566f01f47af69586c63862cbc
abhay <abhay@gmail.com>
2023-09-25 16:06:12 +0530

    aa

--- main.tf
+++ main.tf
@@ -126,16 +126,7 @@ ingress {
   }
 
 }
-
-# # Create an AWS key pair using the generated public key
-# resource "aws_key_pair" "my_key_pair" {
-#   key_name   = "my-key-pair"  # Replace with your desired key name
-#   public_key = file("~/.ssh/my_key_rsa.pub")  # Path to your public SSH key
-# }
-#  resource "aws_key_pair" "example" {
-#   key_name   = "my-key-pair"  # Replace with your desired key name
-#   public_key = file("~/.ssh/id_rsa.pub")  # Replace with the path to your public key file
-#  }
+  
 resource "tls_private_key" "private_key" {
   algorithm = "RSA"
   rsa_bits = "4096"
@@ -154,13 +145,6 @@ resource "aws_instance" "public_instance" {
   key_name               = aws_key_pair.generated_key.key_name # Associate with the key pair
   vpc_security_group_ids = [aws_security_group.React-django.id] # Attach the security group
   # ... other instance configuration ...
-
-  # user_data = <<-EOF
-  #   #!/bin/bash
-  #   ssh-keygen
-  #   chmod 600 ~/.ssh/id_rsa
-  #   chmod 644 ~/.ssh/id_rsa.pub
-  #   EOF
 }
 
 
